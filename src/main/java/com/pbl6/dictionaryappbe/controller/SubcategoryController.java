7fbd8832cf59059cf13a04ba6d0323b4b34b2f21
Huỳnh Lê Đắc Tài <dactai2k2@gmail.com>
2023-10-07 13:47:08 +0700

    DA-BE-29: create Controller and Service class

---
+++ src/main/java/com/pbl6/dictionaryappbe/controller/SubcategoryController.java
@@ -0,0 +1,13 @@
+package com.pbl6.dictionaryappbe.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/subcategories")
+public class SubcategoryController {
+
+
+}
---
+++ src/main/java/com/pbl6/dictionaryappbe/service/SubcategoryServiceImpl.java
@@ -0,0 +1,8 @@
+package com.pbl6.dictionaryappbe.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SubcategoryServiceImpl implements SubcategoryService{
+
+}
