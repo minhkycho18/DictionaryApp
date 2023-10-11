910aad5d0af16e5268526393d2ec3db012d338a1
Nguyễn Quốc Thành <78245193+Nguyenquocthanh268@users.noreply.github.com>
2023-10-11 14:22:58 +0700

    create authentication screen with animation (#31)

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
-import { NavigationContainer } from '@react-navigation/native'
-import BottomTab from './src/navigations/bottomTab';
-import Home from './src/screens/Home';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './src/screens/LoginScreen';
+import SignUpScreen from './src/screens/SignUpScreen';
+
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
- 
-          <BottomTab />
- 
-    // <Home />
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Login' screenOptions={{headerShown: false}}>
+        <Stack.Screen name='Login' component={LoginScreen}/>
+        <Stack.Screen name='SignUp' component={SignUpScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
