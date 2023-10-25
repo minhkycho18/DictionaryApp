17195b8bd95400a615d94813a7a0eec8c72648e9
Minh Van Nhat Dang <80151408+minhkycho18@users.noreply.github.com>
2023-10-25 18:24:01 +0700

    DA-BE-100: Combine api search and detail vocabulary

--- src/main/java/com/pbl6/dictionaryappbe/service/VocabularyService.java
+++ src/main/java/com/pbl6/dictionaryappbe/service/VocabularyService.java
@@ -2,12 +2,8 @@ package com.pbl6.dictionaryappbe.service;
 
 
 import com.pbl6.dictionaryappbe.dto.vocabulary.VocabDetailDto;
-import com.pbl6.dictionaryappbe.dto.vocabulary.VocabularySearchDto;
 import org.springframework.data.domain.Page;
 
-import java.util.List;
-
 public interface VocabularyService {
-    Page<VocabularySearchDto> findByKeyword(String keyword, int offset, int limit);
-    List<VocabDetailDto> getVocabInfo(String word);
+    Page<VocabDetailDto> findByKeyword(String keyword, int offset, int limit);
 }
