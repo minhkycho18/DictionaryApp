448e8d21efddf8ea10331d6f3a9765889201b0b1
Minh Van Nhat Dang <80151408+minhkycho18@users.noreply.github.com>
2023-11-02 13:49:58 +0700

    DA-BE-124: Add field pos to search vocab api

--- src/main/java/com/pbl6/dictionaryappbe/service/VocabularyServiceImpl.java
+++ src/main/java/com/pbl6/dictionaryappbe/service/VocabularyServiceImpl.java
@@ -8,13 +8,16 @@ import com.pbl6.dictionaryappbe.persistence.vocabulary.Vocabulary;
 import com.pbl6.dictionaryappbe.persistence.vocabulary.WordType;
 import com.pbl6.dictionaryappbe.repository.VocabularyRepository;
 import com.pbl6.dictionaryappbe.utils.AuthenticationUtils;
+import jakarta.persistence.criteria.Predicate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -24,40 +27,64 @@ public class VocabularyServiceImpl implements VocabularyService {
     private final VocabularyMapper vocabularyMapper;
 
     @Override
-    public Page<VocabDetailDto> findByKeyword(String keyword, int offset, int limit) {
+    public List<String> findAllPos() {
+        return vocabularyRepository.findAllPos();
+    }
+
+    @Override
+    public Page<VocabDetailDto> findByKeyword(String keyword, String pos, int offset, int limit) {
         int pageNo = offset / limit;
         Pageable pageable = PageRequest.of(pageNo, limit, Sort.by("word").ascending());
-        Page<Vocabulary> vocabularies =
-                vocabularyRepository.findByWordStartsWithAndWordType(keyword, WordType.DEFAULT, pageable);
+
+        Specification<Vocabulary> filterSpec = (root, query, cb) -> {
+            List<Predicate> predicates = new ArrayList<>();
+            if (keyword != null) {
+                predicates.add(cb.like(root.get("word"), keyword + "%"));
+            }
+            if (pos != null) {
+                predicates.add(cb.equal(root.get("pos"), pos.toLowerCase()));
+            }
+            predicates.add(cb.equal(root.get("wordType"), WordType.DEFAULT));
+            return cb.and(predicates.toArray(new Predicate[0]));
+        };
+
+        Page<Vocabulary> vocabularies = vocabularyRepository.findAll(filterSpec, pageable);
         Page<VocabDetailDto> detailDtoList = vocabularies.map(vocabularyMapper::toVocabDetailDto);
 
         User user = AuthenticationUtils.getUserFromSecurityContext();
         if (user != null) {
-            // Get all defId in detailDtoList
-            List<String> defIds = vocabularies.stream()
-                    .flatMap(vocabulary -> vocabulary.getVocabDefs().stream())
-                    .map(vocabDef -> vocabDef.getVocabId().toString() + "-" + vocabDef.getDefId().toString())
-                    .toList();
-            // Get all vocab in user's word list by list defIds
-            List<Long> userWordlistDefIds =
-                    vocabularyRepository.findVocabWordlistByUserEmailAndVocabIds(user.getEmail(), defIds);
-            // Get all vocab in user's leitner by list defIds
-            List<Long> userLeitnerDefIds =
-                    vocabularyRepository.findVocabLeitnerByEmailAndVocabIds(user.getEmail(), defIds);
-
-            // Convert DefinitionDetailDto to DefinitionDetailUserDto
-            detailDtoList.forEach(vocabDetailDto ->
-                    vocabDetailDto.setDefinitions(vocabDetailDto.getDefinitions().stream()
-                            .map(definitionDetailDto ->
-                                    new DefinitionDetailUserDto(
-                                            definitionDetailDto,
-                                            userWordlistDefIds.contains(definitionDetailDto.getDefId()),
-                                            userLeitnerDefIds.contains(definitionDetailDto.getDefId())
-                                    )
-                            )
-                            .toList())
-            );
+            setInfoVocabOfUser(detailDtoList, vocabularies, user);
         }
         return detailDtoList;
+    }
+
+    @Override
+    public void setInfoVocabOfUser(
+            Page<VocabDetailDto> detailDtoList, Page<Vocabulary> vocabularies,  User user
+    ) {
+        // Get all defId in detailDtoList
+        List<String> defIds = vocabularies.stream()
+                .flatMap(vocabulary -> vocabulary.getVocabDefs().stream())
+                .map(vocabDef -> vocabDef.getVocabId().toString() + "-" + vocabDef.getDefId().toString())
+                .toList();
+        // Get all vocab in user's word list by list defIds
+        List<Long> userWordlistDefIds =
+                vocabularyRepository.findVocabWordlistByUserEmailAndVocabIds(user.getEmail(), defIds);
+        // Get all vocab in user's leitner by list defIds
+        List<Long> userLeitnerDefIds =
+                vocabularyRepository.findVocabLeitnerByEmailAndVocabIds(user.getEmail(), defIds);
+
+        // Convert DefinitionDetailDto to DefinitionDetailUserDto
+        detailDtoList.forEach(vocabDetailDto ->
+                vocabDetailDto.setDefinitions(vocabDetailDto.getDefinitions().stream()
+                        .map(definitionDetailDto ->
+                                new DefinitionDetailUserDto(
+                                        definitionDetailDto,
+                                        userWordlistDefIds.contains(definitionDetailDto.getDefId()),
+                                        userLeitnerDefIds.contains(definitionDetailDto.getDefId())
+                                )
+                        )
+                        .toList())
+        );
     }
 }
