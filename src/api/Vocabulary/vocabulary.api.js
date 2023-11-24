1a39533e3366c6e5942db7bf09b7d1fe6847d6d0
Nhật Minh <80151408+minhkycho18@users.noreply.github.com>
2023-11-24 22:28:13 +0700

    DA-BE-175:  Edit delete vocab in management page (#119)
    * DA-FE-175: Edit and delete vocab in management page
    
    * DA-FE-175: Fix conflict code

--- src/api/Vocabulary/vocabulary.api.js
+++ src/api/Vocabulary/vocabulary.api.js
@@ -4,3 +4,11 @@ import http from "../../helpers/http";
 export const getAllPos = () => {
     return http.get(`/vocabs/pos`);
 };
+
+export const updateDefaultVocab = (vocabId, data) => {
+    return http.put(`http://localhost:8080/managements/vocabs/${vocabId}`,data);
+};
+
+export const deleteDefaultVocab = (vocabId) => {
+    return http.delete(`http://localhost:8080/managements/vocabs/${vocabId}`);
+};
