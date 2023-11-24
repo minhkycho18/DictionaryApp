67cfa76fdb386bbbd437f08aa3d2abd4d38dc08b
Nhật Minh <80151408+minhkycho18@users.noreply.github.com>
2023-11-24 16:14:28 +0700

    DA-BE-183:  Change logic code update default vocabulary (#117)
    * DA-BE-183: Fix bug update vocab
    
    * DA-BE-183: Remove only full group by mode in mysql container

--- src/main/java/com/pbl6/dictionaryappbe/service/VocabularyServiceImpl.java
+++ src/main/java/com/pbl6/dictionaryappbe/service/VocabularyServiceImpl.java
@@ -118,9 +118,16 @@ public class VocabularyServiceImpl implements VocabularyService {
     public void updateDefaultVocab(Long vocabId, UpdateDefaultVocabRequest updateDefaultVocabRequest) {
         Vocabulary vocabulary = vocabularyRepository.findById(vocabId)
                 .orElseThrow(() -> new RecordNotFoundException("Vocabulary not found"));
+        // case delete def
+        List<String> currentVocabDefs = updateDefaultVocabRequest.getDefinitions().stream()
+                .filter(definitionShortDetail -> definitionShortDetail.getDefId() != null)
+                .map(vocabDef -> vocabId + "-" + vocabDef.getDefId())
+                .toList();
+        vocabDefRepository.updateStatusVocabs(currentVocabDefs, vocabId, true);
+
         vocabulary.setAudioUs(updateDefaultVocabRequest.getAudioUs());
         vocabulary.setAudioUk(updateDefaultVocabRequest.getAudioUk());
-        vocabulary.setPhoneUs(updateDefaultVocabRequest.getAudioUs());
+        vocabulary.setPhoneUs(updateDefaultVocabRequest.getPhoneUs());
         vocabulary.setPhoneUk(updateDefaultVocabRequest.getPhoneUk());
         updateDefaultVocabRequest.getDefinitions().forEach(definitionShortDetail -> {
             if (definitionShortDetail.getDefId() != null) {
@@ -147,12 +154,6 @@ public class VocabularyServiceImpl implements VocabularyService {
                 vocabDefRepository.save(vocabDef);
             }
         });
-        // case delete def
-        List<String> currentVocabDefs = updateDefaultVocabRequest.getDefinitions().stream()
-                .filter(definitionShortDetail -> definitionShortDetail.getDefId() != null)
-                .map(vocabDef -> vocabId + "-" + vocabDef.getDefId())
-                .toList();
-        vocabDefRepository.updateStatusVocabs(currentVocabDefs, vocabId, true);
     }
 
 
