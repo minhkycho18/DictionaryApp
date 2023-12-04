2a1c95c4ee2e26098fb90fcb09b31a0335273f56
Nguyenquocthanh268 <Nguyenquocthanh268@gmail.com>
2023-12-05 02:14:49 +0700

    commit 1

--- src/api/Leitner.js
+++ src/api/Leitner.js
@@ -6,3 +6,6 @@ export const getInforBoxOfUser = () => {
 export const addVocalToLeitner = (data) => {
     return http.post(`/leitners`, data)
 }
+export const getVocabOfLeitnerLevelOfUser = (level,offset = 0) => {
+    return http.get(`/leitners/${level}?offset=${offset}&limit=5`);
+};
